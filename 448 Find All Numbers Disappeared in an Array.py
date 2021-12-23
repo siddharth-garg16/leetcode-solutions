771a2373b6a0afd6c269ad012362843e50916734
Siddharth Garg <80592648+siddharthg16@users.noreply.github.com>
2021-12-23 18:32:45 +0530

    Add files via upload

---
+++ 448 Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findDisappearedNumbers(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        disappearedNumbers=[]
+        i=0
+        while i<len(arr):
+            correctIndex=arr[i]-1
+            if arr[i]!=arr[correctIndex]:
+                arr[i],arr[correctIndex]=arr[correctIndex],arr[i]
+            else:
+                i=i+1
+                
+        for i in range(len(arr)):
+            if arr[i]!=i+1:
+                disappearedNumbers.append(i+1)
+                
+        return disappearedNumbers
