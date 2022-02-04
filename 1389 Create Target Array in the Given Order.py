813b1b2d462c52fd4e5a7a42e3fe4dbd8ffa21d5
Siddharth Garg <80592648+siddharthg16@users.noreply.github.com>
2022-02-04 20:47:52 +0530

    Add files via upload

---
+++ 1389 Create Target Array in the Given Order.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        #---using in-built insert function---
+        # target = []
+        # for i in range(0, len(nums)):
+        #     target.insert(index[i], nums[i])
+            
+        # return target
+
+
+
+        #---using list slicing---
+        target = []
+        for i in range(0, len(nums)):
+            if index[i] == len(target):
+                target.append(nums[i])
+            else:
+                target =target[:index[i]] + [nums[i]] + target[index[i]:]
+                
+        return target
