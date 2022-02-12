921e95cec7fb5242a63924205d2622b646aa2d32
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2022-02-12 10:44:06 +0530

    Add files via upload

--- 1295 Find Numbers with Even Number of Digits.py
+++ 1295 Find Numbers with Even Number of Digits.py
@@ -4,9 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count=0
-        for number in nums:
-            if int(math.log10(number)+1) % 2 == 0:
-                count=count+1
-            
+        count = 0
+
+        #---method1---
+        # for i in range(len(nums)):
+        #     if len(str(nums[i]))%2==0:
+        #         count+=1
+        # return count
+
+
+        #---method2---
+        for num in nums:
+            if int(math.log10(num))%2==1:
+                count+=1
+                
         return count
