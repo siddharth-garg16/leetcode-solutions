8dfde7fbf28f29f5d0dff43eb7d2e2411f63c8ce
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2022-03-24 14:38:41 +0530

    Add files via upload

---
+++ 875. Koko Eating Bananas.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low, high = 1, 10 ** 9
+        while low <= high:
+            k = (low + high) // 2
+            if sum(math.ceil(pile / k) for pile in piles) > h:
+                low = k + 1
+            else:
+                high = k - 1
+
+        return low
