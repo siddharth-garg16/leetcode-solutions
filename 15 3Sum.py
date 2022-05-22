514ce327d24cc89db299e3b74b36a883bf5cb5ca
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2022-05-22 14:38:43 +0530

    Add files via upload

---
+++ 15 3Sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        res = []
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+                threesum = a + nums[j] + nums[k]
+                if threesum < 0:
+                    j += 1
+                elif threesum > 0:
+                    k -= 1
+                else:
+                    res.append([a, nums[j], nums[k]])
+                    j += 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+
+        return res
