e9962fec4dc78487e2df9c0f51e0c27f2ccb51fe
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2022-06-15 13:36:46 +0530

    Add files via upload

---
+++ 561 Array Partition I.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        maxsum = 0
+        for i in range(0, len(nums) - 1, 2):
+            maxsum += nums[i]
+
+        return maxsum
